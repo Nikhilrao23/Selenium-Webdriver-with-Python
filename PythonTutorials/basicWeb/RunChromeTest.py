d60ee49d0d5020eaad6a51076250bb2003d0a544
Nikhilrao23 <raonikhil23@gmail.com>
2020-01-23 18:05:15 -0500

    Running basic Chrome Test

---
+++ PythonTutorials/basicWeb/RunChromeTest.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+import os
+
+
+class RunChromeTest():
+
+    def test(self):
+
+        driverLocation = "/Users/nikhilraobalaji/PycharmProjects/PythonTutorials/chromedriver"
+        os.environ["webdriver.chrome.driver"] = driverLocation
+        driver = webdriver.Chrome(driverLocation)
+
+        driver.get("https://www.google.com")
+
+ff = RunChromeTest()
+ff.test()
+
+"""
+Firefox
+1. Install Geckodriver
+2. driver = webdriver.Firefox(executable_path = " ")
+3. 
+"""
